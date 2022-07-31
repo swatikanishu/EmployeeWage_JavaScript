af6db7effd1f3700c86dbf39877b15d3a2f714d5
swatikanishu <nishuswatika@gmail.com>
2022-07-31 22:08:06 +0530

    Added:  added code to find the emp wage for month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,6 +5,7 @@ const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 const IS_ABSENT = 0;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
 switch(empCheck) {
@@ -18,8 +19,10 @@ switch(empCheck) {
             return 0;
 }
 }
-let empHours = 0 ;
+let empHours = 0;
+for(let day = 0; day <= NUM_OF_WORKING_DAYS; day++) {
 let empCheck = Math.floor(Math.random() * 10) % 3;
-empHours = getWorkingHours(empCheck);
+empHours = empHours + getWorkingHours(empCheck);
+}
 let empWage = empHours * WAGE_PER_HOUR;
-console.log("emp wage is " + empWage);
+console.log("Total hour " + empHours + " emp wage is " + empWage);
