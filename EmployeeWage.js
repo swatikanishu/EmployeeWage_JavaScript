c7c707d89a0470bb11e8a2f2d7ae4f3f9c27c902
swatikanishu <nishuswatika@gmail.com>
2022-07-31 22:22:52 +0530

    Added:added code to find the emp wage for max working day and max working hour

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,6 +6,7 @@ const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 const IS_ABSENT = 0;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOUR_IN_MONTH = 100;
 
 function getWorkingHours(empCheck) {
 switch(empCheck) {
@@ -19,10 +20,12 @@ switch(empCheck) {
             return 0;
 }
 }
-let empHours = 0;
-for(let day = 0; day <= NUM_OF_WORKING_DAYS; day++) {
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHours = empHours + getWorkingHours(empCheck);
+let totalEmpHour = 0;
+let totalWorkingDays = 0;
+while(totalEmpHour <= MAX_HOUR_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHour = totalEmpHour + getWorkingHours(empCheck);
 }
-let empWage = empHours * WAGE_PER_HOUR;
-console.log("Total hour " + empHours + " emp wage is " + empWage);
+let empWage = totalEmpHour * WAGE_PER_HOUR;
+console.log("Total hour " + totalEmpHour + " total working day " + totalWorkingDays + " emp wage is " + empWage);
