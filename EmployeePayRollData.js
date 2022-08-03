3f7761fc8e740f2cb2602dc0734e0db23e278a71
swatikanishu <nishuswatika@gmail.com>
2022-08-04 01:34:59 +0530

    MODIFIED: Regex for incorrect name

--- EmployeePayRollData.js
+++ EmployeePayRollData.js
@@ -21,7 +21,11 @@ class EmployeePayrollData {
     }
 
     set name(name) {
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if(nameRegex.test(name))
+            this._name = name;
+        else
+            throw '**** NAME is Incorrect ****';
     }
 
     get salary() {
@@ -57,9 +61,16 @@ class EmployeePayrollData {
 
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
-employeePayrollData.id = 0;
-employeePayrollData.name = "Jeff";
-console.log(employeePayrollData.toString());
+
+try{
+    employeePayrollData.id = 0;
+    employeePayrollData.name = "Joe";
+    console.log(employeePayrollData.toString());
+}
+catch(e){
+    console.error(e);
+}
+
 
 let teresaPayrollData = new EmployeePayrollData(1, "Teresa", 50000, "F", new Date());
 console.log(teresaPayrollData.toString());
