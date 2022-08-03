ecfcc850d7afc6c975f6787031e49c342a6968b3
swatikanishu <nishuswatika@gmail.com>
2022-08-04 01:24:33 +0530

    Added:extended employeepayroll data

--- EmployeePayRollData.js
+++ EmployeePayRollData.js
@@ -1,38 +1,57 @@
-class EmployeePayrollData{
+class EmployeePayrollData {
 
-    constructor(id, name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
-    get id(){
+    get id() {
         return this._id;
     }
 
-    set id(id){
+    set id(id) {
         this._id = id;
     }
 
-    get name(){
+    get name() {
         return this._name;
     }
 
-    set name(name){
-        console.log("Setting name");
+    set name(name) {
         this._name = name;
     }
 
-    get salary(){
+    get salary() {
         return this._salary;
     }
 
-    set salary(salary){
+    set salary(salary) {
         this._salary = salary;
     }
 
-    toString(){
-        return "Id = " + this.id + ", Name = " + this.name + ", Salary = " + this.salary;
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ", Name = " + this.name + ", Gender = " + this.gender + ", Salary = " + this.salary + ", Start Date = " + employeeDate;
     }
 }
 
@@ -41,3 +60,6 @@ console.log(employeePayrollData.toString());
 employeePayrollData.id = 0;
 employeePayrollData.name = "Jeff";
 console.log(employeePayrollData.toString());
+
+let teresaPayrollData = new EmployeePayrollData(1, "Teresa", 50000, "F", new Date());
+console.log(teresaPayrollData.toString());
